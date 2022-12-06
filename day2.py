0c45b2c2ccab6b843a6b2f0891a1742af1883474
Rafaël Beckers <r.m.a.beckers@student.tudelft.nl>
2022-12-06 16:37:57 +0100

    a

--- day2.py
+++ day2.py
@@ -6,9 +6,72 @@ data = '\day2.txt'
 path = Path.cwd()
 path = str(path) + data
 
-data = open(path, "r")
+data = np.loadtxt(path, dtype=str)
 
 #Part 1
+score = 0
 #A,X = rock, B,Y = paper, C,Z = scissors
-for i, d in enumerate(data):
-    print(d[2])
+for round_ in  data:
+    opp = round_[0]
+    me = round_[1]
+    
+    if opp == 'A':
+        if me == 'X':
+            score += 3 + 1
+        elif me == 'Y':
+            score += 6 + 2
+        else:
+            score += 3
+        
+    elif opp == 'B':
+        if me == 'X':
+            score += 0 + 1
+        elif me == 'Y':
+            score += 3 + 2
+        else:
+            score += 6 + 3
+    
+    else:
+        if me == 'X':
+            score += 6 + 1
+        elif me == 'Y':
+            score += 0 + 2
+        else:
+            score += 3 + 3
+        
+    
+print(score)
+
+# Part 2
+score = 0
+#A = rock, B = paper, C = scissors
+#X = lose, Y = Draw, Z = win
+for round_ in  data:
+    opp = round_[0]
+    me = round_[1]
+    
+    if me == 'X':
+        if opp == 'A':
+            score += 3
+        elif opp == 'B':
+            score += 1
+        else:
+            score += 2
+        
+    elif me == 'Y':
+        if opp == 'A':
+            score += 3 + 1
+        elif opp == 'B':
+            score += 3 + 2
+        else:
+            score += 3 + 3
+    
+    else:
+        if opp == 'A':
+            score += 6 + 2
+        elif opp == 'B':
+            score += 6 + 3
+        else:
+            score += 6 + 1
+            
+print(score)
